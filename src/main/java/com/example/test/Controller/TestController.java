aaa36e4b72a7e9571b8e436b4d872c34e046b981
3019244337-Liu Gelun <476823412@qq.com>
2022-07-25 10:01:34 +0800

    1

--- src/main/java/com/example/test/Controller/TestController.java
+++ src/main/java/com/example/test/Controller/TestController.java
@@ -1,18 +1,22 @@
 package com.example.test.Controller;
 
+import com.alibaba.fastjson2.JSON;
 import com.example.test.Util.BillType;
 import com.example.test.Util.Result;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 @RestController
-@RequestMapping("/1")
+@RequestMapping()
 public class TestController {
     @GetMapping("/test")
-    public static Result test(){
+    public static Result test(HttpServletRequest request){
+        //String str = JSON.toJSONString(request);
+        System.out.println(request.getLocalAddr());
         List<BillType> list = new ArrayList<>();
         List<BillType> list2 = new ArrayList<>();
         for (int i = 0; i< 5; i++){
